654bef4e08873ff65dcb2e72cee6dc287184150d
Harsh Bhargava <harshbhargav11966@example.com>
2025-10-21 20:34:14 +0530

    Add counter and hello_world projects

---
+++ task1/RNG_game/guessing_game/target/debug/deps/cfg_if-43f8c950438ad461.d
@@ -0,0 +1,7 @@
+/home/harsh/Documents/TurbinePB_Q425_Harshbhargav45/task1/RNG_game/guessing_game/target/debug/deps/cfg_if-43f8c950438ad461.d: /home/harsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
+
+/home/harsh/Documents/TurbinePB_Q425_Harshbhargav45/task1/RNG_game/guessing_game/target/debug/deps/libcfg_if-43f8c950438ad461.rlib: /home/harsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
+
+/home/harsh/Documents/TurbinePB_Q425_Harshbhargav45/task1/RNG_game/guessing_game/target/debug/deps/libcfg_if-43f8c950438ad461.rmeta: /home/harsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs
+
+/home/harsh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.4/src/lib.rs:
---
+++ task1/RNG_game/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/harsh/Documents/TurbinePB_Q425_Harshbhargav45/task1/RNG_game/guessing_game/target/debug/guessing_game: /home/harsh/Documents/TurbinePB_Q425_Harshbhargav45/task1/RNG_game/guessing_game/src/main.rs
